682fd6e6117d1413ab51933a6382014e511f085d
hifoxwei <znwlove@gmail.com>
2020-11-05 13:15:38 +0800

    complete

--- src/components/Voice/index.js
+++ src/components/Voice/index.js
@@ -8,11 +8,18 @@ import './index.css';
 export default function Text(props = {}) {
   const { dataSource: { playload: { duration }, userInfo: { avatar, nick } } } = props;
   return (
-    <div className="group-chat-card-text">
+    <div className="group-chat-card-voice">
       <Avatar dataSource={avatar} />
       <div>
         <Nick dataSource={nick} />
-        <div className="group-chat-card-text-content">{duration}</div>
+        <div className="group-chat-card-voice-wrapper">
+          <div className="group-chat-card-voice-content">
+            <img className="group-chat-card-voice-icon" src="../assets/icons/voice-lite.png" />
+            {duration}‘’
+          </div>
+          <img className="group-chat-card-voice-red-dot" src="../assets/icons/red-dot.png" />
+          <div className="group-chat-card-voice-text">转换成文字</div>
+        </div>
       </div>
     </div>
   );
